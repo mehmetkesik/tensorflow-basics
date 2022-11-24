10484c9485041e37a9fd02a2f4fb61957f558cbe
mehmetkesik <52535909+mehmetkesik@users.noreply.github.com>
2022-11-24 13:17:18 +0300

    Helper functions for TensorFlow.

--- helper_functions.py
+++ helper_functions.py
@@ -286,3 +286,26 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
+
+# View random image from folder
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+
+def view_random_image(target_dir, target_class):
+  # Setup the target directory (we'll view images from here)
+  target_folder = target_dir+target_class
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+  print(random_image)
+
+  # Read in the image and plot it using matplotlib
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off")
+
+  print(f"Image shape: {img.shape}") # Show the shape of the image
+
+  return img
